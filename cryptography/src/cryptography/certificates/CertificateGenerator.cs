66f945a8da862fc5bf749d9df6d6508766f6edb0
Josh Rolstad <jrolstad@gmail.com>
2016-11-28 15:15:59 -0800

    Adding certificate info

---
+++ cryptography/src/cryptography/certificates/CertificateGenerator.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace cryptography.certificates
+{
+    public class CertificateGenerator
+    {
+        public X509Certificate GenerateNewCertificate()
+        {
+
+            var certificate =  new X509Certificate();
+
+            return certificate;
+        }
+    }
+}
