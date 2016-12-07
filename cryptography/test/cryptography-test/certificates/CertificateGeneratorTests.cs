60562b2c1becc247af2b460451e79819c5fd4d37
Josh Rolstad <jrolstad@gmail.com>
2016-12-07 09:51:13 -0800

    Adding tests around getting certificates from a store

---
+++ cryptography/test/cryptography-test/certificates/CertificateGeneratorTests.cs
@@ -0,0 +1,34 @@
+using cryptography.certificates;
+using Xunit;
+using System.Security.Cryptography.X509Certificates;
+using System;
+
+namespace cryptography_test.certificates
+{
+    public class CertificateGeneratorTest
+    {
+
+
+        [Fact]
+        public void ReadCerts()
+        {
+            using(var store = new X509Store("MY",StoreLocation.CurrentUser))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                 X509Certificate2Collection collection = (X509Certificate2Collection)store.Certificates;
+
+                Console.WriteLine("Number of certificates: {0}{1}",collection.Count,Environment.NewLine);
+
+                Console.WriteLine("Reading Certs");
+                foreach(var cert in store.Certificates)
+                {
+                    Console.WriteLine("FriendlyName:{0}",cert.FriendlyName);
+                    Console.WriteLine("KeyAlgorithm:{0}",cert.GetKeyAlgorithm());
+                     Console.WriteLine("IssuerName:{0}",cert.IssuerName.Name);
+                }
+            }
+
+           
+        }
+    }
+}
