b7942c86f552385505ec2be6dccf0c092b17fdb5
Josh Rolstad <jorolsta@microsoft.com>
2021-01-22 10:18:20 -0800

    Showing differences between different function versions

---
+++ net5AzureFunction/fiveFunction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Worker.Configuration;
+
+namespace fiveFunction
+{
+    // take from https://github.com/Azure/azure-functions-dotnet-worker-preview
+
+    // To run from cli: func host start --verbose
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //#if DEBUG
+            //             Debugger.Launch();
+            //#endif
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(c =>
+                {
+                    c.AddCommandLine(args);
+                })
+                .ConfigureFunctionsWorker((c, b) =>
+                {
+                    b.UseFunctionExecutionMiddleware();
+                })
+                .ConfigureServices(s =>
+                {
+                    //s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
---
+++ net5AzureFunction/fiveFunction/Services/HttpResponderService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fiveFunction.Services
+{
+    public interface IHttpResponderService
+    {
+        HttpResponseData ProcessRequest(HttpRequestData httpRequest);
+    }
+
+    public class DefaultHttpResponderService : IHttpResponderService
+    {
+        public DefaultHttpResponderService()
+        {
+
+        }
+
+        public HttpResponseData ProcessRequest(HttpRequestData httpRequest)
+        {
+            var response = new HttpResponseData(HttpStatusCode.OK);
+            var headers = new Dictionary<string, string>();
+            headers.Add("Date", "Mon, 18 Jul 2016 16:06:00 GMT");
+            headers.Add("Content", "Content - Type: text / html; charset = utf - 8");
+
+            response.Headers = headers;
+            response.Body = "Welcome to .NET 5!!";
+
+            return response;
+        }
+    }
+}
