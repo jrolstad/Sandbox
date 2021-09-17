d56d964d39e864b38b9c53f8a6737c9f13fb275b
Josh Rolstad <jorolsta@microsoft.com>
2021-09-16 19:25:08 -0700

    Stubbing out tests

---
+++ unit-test-demo/MyApp.Tests/StringServiceTests.cs
@@ -0,0 +1,56 @@
+using System;
+using Xunit;
+
+namespace MyApp.Tests
+{
+    public class StringServiceTests
+    {
+        [Fact]
+        public void Reverse_null_ReturnsNull()
+        {
+            var service = new StringService();
+
+            string input = null;
+            var result = service.Reverse(input);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Reverse_Empty_ReturnsEmpty()
+        {
+            var service = new StringService();
+
+            string input = "";
+            var result = service.Reverse(input);
+
+            Assert.Equal("",result);
+        }
+
+        [Fact]
+        public void Reverse_ValidValue_ReturnsReversedString()
+        {
+            var service = new StringService();
+
+            string input = "hello";
+            var result = service.Reverse(input);
+
+            Assert.Equal("olleh", result);
+        }
+
+        [Theory] // See https://spr.com/keeping-xunit-tests-clean-and-dry-using-theory/
+        [InlineData(null,null)]
+        [InlineData("","")]
+        [InlineData("stephen","nehpets")]
+        [InlineData("data","atad")]
+        [InlineData("tacocat","tacocat")]
+        public void Reverse_ReversesString(string input, string expected)
+        {
+            var service = new StringService();
+
+            var result = service.Reverse(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ unit-test-demo/MyApp/StringService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace MyApp
+{
+    public class StringService
+    {
+        public string Reverse(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            var reversedArray = input
+                .ToCharArray()
+                .Reverse()
+                .ToArray();
+
+            return new string(reversedArray);
+        }
+    }
+}
