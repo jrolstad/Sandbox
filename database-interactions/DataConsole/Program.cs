57a794e9adf81e98a4011c07c29baa9e65f0a82d
Josh Rolstad <jorolsta@microsoft.com>
2021-10-15 14:07:33 -0700

    Adding sample code for interacting with a database

---
+++ database-interactions/DataConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using DataConsole.Repositories;
+using System;
+using System.IO;
+
+namespace DataConsole
+{
+    public class Program
+    {
+        private readonly MessageRepository _messageRepository;
+        private readonly TextReader _input;
+        private readonly TextWriter _output;
+
+        static void Main(string[] args)
+        {
+            var program = new Program(new MessageRepository(),Console.In,Console.Out);
+
+            program.Run(args);
+        }
+
+        public Program(MessageRepository messageRepository,TextReader input, TextWriter output)
+        {
+            _messageRepository = messageRepository;
+            _input = input;
+            _output = output;
+        }
+
+        public void Run(string[] args)
+        {
+            _messageRepository.Configure();
+
+            string messageInput = null;
+            do
+            {
+                _output.WriteLine("----Enter your message");
+                messageInput = _input.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(messageInput))
+                {
+                    _messageRepository.Save(messageInput);
+
+                    var messages = _messageRepository.Get();
+
+                    _output.WriteLine("----Saved Messages");
+                    foreach (var item in messages)
+                    {
+                        _output.WriteLine(item);
+                    }
+                }
+
+
+            } while (!string.IsNullOrWhiteSpace(messageInput));
+            
+
+        }
+    }
+}
---
+++ database-interactions/DataConsole/Repositories/MessageRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace DataConsole.Repositories
+{
+    public class MessageRepository
+    {
+        public void Configure()
+        {
+            using var connection = new SqliteConnection("Data Source=hello.db");
+            connection.Open();
+
+            var existingCommand = connection.CreateCommand();
+            existingCommand.CommandText = "SELECT name FROM sqlite_master WHERE name='message'";
+            var existing = existingCommand.ExecuteScalar();
+
+            if (existing!=null) return;
+
+            var createCommand = connection.CreateCommand();
+            createCommand.CommandText = @"CREATE TABLE message (messagetext VARCHAR(100))";
+            createCommand.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        public void Save(string message)
+        {
+            using var connection = new SqliteConnection("Data Source=hello.db");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "insert into message (messagetext) values ($newMessage)";
+            command.Parameters.AddWithValue("$newMessage", message);
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        public List<string> Get()
+        {
+            using var connection = new SqliteConnection("Data Source=hello.db");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "select messagetext from message";
+
+            using var reader = command.ExecuteReader();
+
+            var result = new List<string>();
+            while (reader.Read())
+            {
+                var message = reader.GetString(0);
+                result.Add(message);
+            }
+
+            connection.Close();
+
+            return result;
+        }
+    }
+}
